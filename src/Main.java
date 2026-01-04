99606680dc5e800cfd2fe719cde37a5194570199
00MOREIRA00 <robertocorreamoreiraneto@gmail.com>
2026-01-04 20:50:58 -0300

    feat: adicionando código de types

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
+import playground.DataTypes;
+
 public class Main {
     public static void main(String[] args) {
 
-        String minhaString = "Hellow, World!";
-        int meuInteger = 31;
-
-        System.out.println(minhaString);
-        System.out.println("O Desenvolvedor que está aqui nasceu no dia " + meuInteger);
+        DataTypes.datatypes();
     }
 }
---
+++ src/playground/DataTypes.java
@@ -0,0 +1,11 @@
+package playground;
+
+public class DataTypes {
+    public static void datatypes() {
+        String minhaString = "Hellow, World!";
+        int meuInteger = 31;
+
+        System.out.println(minhaString);
+        System.out.println("O Desenvolvedor que está aqui nasceu no dia " + meuInteger);
+    }
+}
