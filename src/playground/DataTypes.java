eb1da59e091e833fefcdf9e98938d9ab5249fac3
00MOREIRA00 <robertocorreamoreiraneto@gmail.com>
2026-01-05 23:35:39 -0300

    feat: adicionando um boolean

--- src/playground/DataTypes.java
+++ src/playground/DataTypes.java
@@ -4,8 +4,10 @@ public class DataTypes {
     public static void datatypes() {
         String minhaString = "Hellow, World!";
         int meuInteger = 31;
+        boolean meuBollean = true;
 
         System.out.println(minhaString);
         System.out.println("O Desenvolvedor que está aqui nasceu no dia " + meuInteger);
+        System.out.println("Esse projeto está rodando ? " + meuBollean);
     }
 }
